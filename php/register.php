87ab289e28aaf040445c308414c9a4a38f42bb43
dicheto <58654601+dicheto@users.noreply.github.com>
2024-11-11 22:10:41 +0200

    Added the front-end with form
    Connection to the db is successfully estabilished

--- php/register.php
+++ php/register.php
@@ -26,3 +26,97 @@ if ($_POST) { // Check if the form has been submitted
     }
 }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8"> <!-- Set character encoding for the document -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- Set viewport for responsive design -->
+    <title>Лог-ин</title> <!-- Set the title of the page -->
+    <link rel="stylesheet" href="/css/style.css"> <!-- Link to the custom stylesheet -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet"> <!-- Link to AOS CSS for animations -->
+    <style>
+        @media (max-width:500px) {
+            .w-50 {
+                width: 75% !important; // Adjust width for smaller screens
+            }
+        }
+    </style>
+</head>
+
+<body class="index">
+    <div data-aos="fade-up" data-aos-duration="1500"
+        class="d-flex w-50 my-5 mx-auto box-shadow-custom border-custom justify-content-end row">
+        <!-- Flex container for layout -->
+        <div class="col-12 col-lg d-flex align-items-center justify-content-center">
+            <a href="../index.php" data-bs-toggle="tooltip" data-bs-placement="right" role="button"
+                data-bs-title="Начало" data-bs-custom-class="custom-tooltip"
+                class="header_icon d-flex align-items-center justify-content-center menu-item-hover"
+                style="color: #fff;">
+                <ion-icon name="home-outline" class="me-1"></ion-icon> <!-- Home icon -->
+            </a>
+        </div>
+        <div class="col-12 col-lg d-flex align-items-center justify-content-center">
+            <p style="color: white;" class="text-center m-0 welcomeGreeting">
+                <?php if (isset($_SESSION['user_id'])) { // Check if user is logged in
+                        echo $result_welcomeGreeting . ", " . htmlspecialchars($_SESSION['first_name']) . ' ' . htmlspecialchars($_SESSION['last_name']); // Display welcome message with user's name
+                    } else {
+                        echo $result_welcomeGreeting . "!"; // Display welcome message for guests
+                    } ?>
+            </p>
+        </div>
+        <div class="col-12 col-lg d-flex justify-content-around">
+            <?php if (!isset($_SESSION['user_id'])): // If user is not logged in ?>
+                <a href="/php/register.php" data-bs-toggle="tooltip" data-bs-placement="right" role="button"
+                    data-bs-title="Регистрация" data-bs-custom-class="custom-tooltip"
+                    class="header_icon menu-item-hover d-flex align-items-center justify-content-center"
+                    style="color: #fff;">
+                    <ion-icon name="person-circle-outline" class="mt-4 mb-3"></ion-icon> <!-- Registration icon -->
+                </a>
+            <?php endif ?>
+            <?php if (isset($_SESSION['user_id'])): // If user is logged in ?>
+                <a href="/php/dashboard.php" data-bs-toggle="tooltip" data-bs-placement="right" role="button"
+                    data-bs-title="Вашите линкове" data-bs-custom-class="custom-tooltip"
+                    class="header_icon menu-item-hover d-flex align-items-center" style="color: #fff;">
+                    <ion-icon name="clipboard-outline" class="me-1"></ion-icon><span class="menu-item"></span>
+                    <!-- Dashboard icon -->
+                </a>
+                <a href="/php/logout.php" data-bs-toggle="tooltip" data-bs-placement="right" role="button"
+                    data-bs-title="Изход" data-bs-custom-class="custom-tooltip"
+                    class="header_icon menu-item-hover ms-4 d-flex align-items-center" style="color: #fff;">
+                    <ion-icon name="exit-outline" class="me-1"></ion-icon><span class="menu-item"></span>
+                    <!-- Logout icon -->
+                </a>
+            <?php endif; ?>
+        </div>
+    </div>
+    <div data-aos="fade-up" data-aos-duration="1500"
+        class="mb-5 container align-items-center d-flex justify-content-center">
+        <div class="w-50 p-3 p-md-4 box-shadow-custom border-custom bg-white text-center">
+            <!-- Container for registration form -->
+            <h2 class="text-center mb-0">Регистрация</h2> <!-- Registration title -->
+            <p class="mt-0">Имаш регистрация? <a style="text-decoration: underline; color: black;;"
+                    href="/php/login.php">Влез</a></p> <!-- Link to login page -->
+            <form method="POST" class="my-4"> <!-- Form for user registration -->
+                <?php if (isset($error_register)) { // Check if there is an error message
+                        echo $error_register; // Display the error message
+                    }
+                    $error_register = "" ?> <!-- Reset error message -->
+                <input type="text" name="first_name" placeholder="Име" class="mx-auto mb-2 w-100 pe-1" required>
+                <!-- Input for first name -->
+                <input type="text" name="last_name" placeholder="Фамилия" class="mx-auto mb-2 w-100 pe-1" required>
+                <!-- Input for last name -->
+                <input type="text" name="username" placeholder="Имейл адрес" class="mx-auto mb-2 w-100 pe-1" required>
+                <!-- Input for username (email) -->
+                <input type="password" name="password" placeholder="Парола" class="mx-auto mb-3 w-100 pe-1" required>
+                <!-- Input for password -->
+
+                <button type="submit" class="btn-submit px-3 d-flex justify-content-center">Регистриране</button>
+                <!-- Submit button for registration -->
+            </form>
+        </div>
+    </div>
+</body>
+</html>
