9141a2eb601646332539b31df8aeb24a5bb92be3
dicheto <58654601+dicheto@users.noreply.github.com>
2024-11-11 22:11:33 +0200

    Added libraries to shorten.php
    Added QR library to shorten.php

---
+++ php/shorten.php
@@ -0,0 +1,4 @@
+<?php
+require '../config.php'; // Include the configuration file for database connection
+include '../php/time.php'; // Include the time-related functions
+include '../phpqrcode/qrlib.php'; // Include the QR code library
