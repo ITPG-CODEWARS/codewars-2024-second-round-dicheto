972cd4b7870535649c454fd5c78570f087ca5ba6
dicheto <58654601+dicheto@users.noreply.github.com>
2024-11-11 21:55:51 +0200

    Added a filter for accounts with no links

--- php/dashboard.php
+++ php/dashboard.php
@@ -97,6 +97,10 @@ $links = $stmt->fetchAll(PDO::FETCH_ASSOC); // Fetch all links as an associative
                 <p class="text-center my-3" style="color: green;"><?php echo $deleteMessage; ?></p>
                 <!-- Display delete message if set -->
             <?php endif; ?>
+
+            <?php if (count($links) == 0) { ?> <!-- Check if there are no links -->
+                <p class="text-center my-3">Нямате линкове свързани с акаунта ви</p> <!-- Message for no links -->
+            <?php } ?>
                         <div class="col-1 px-0"> <!-- Column for delete button -->
                             <a href="/php/dashboard.php?delete_id=<?php echo $link['id']; ?>" class="delete-btn"
                                 onclick="return confirm('Сигурни ли сте, че искате да изтриете този линк?');"><ion-icon
